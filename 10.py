03baed74d350043ae25fed282a7978f9ed6a4df6
Alexey Shamrin <shamrin@gmail.com>
2020-12-10 09:53:41 +0200

    day 10

---
+++ 10.py
@@ -0,0 +1,34 @@
+I = [int(line) for line in  open('input10.txt') if line]
+
+I.sort()
+I = [0] + I + [max(I) + 3]
+
+# part 1
+ones = 0
+threes = 0
+for i in range(1, len(I)):
+    d = I[i] - I[i-1]
+    if d == 1:
+        ones += 1
+    if d == 3:
+        threes += 1
+print(ones*threes)
+
+# part 2
+ds = [0] + [I[i] - I[i-1] for i in range(1, len(I))]
+c = 1
+ones = 0
+for d in ds:
+    if d == 1:
+        ones += 1
+    else:
+        if ones == 2:
+            c *= 2
+        elif ones == 3:
+            c *= 4
+        elif ones == 4:
+            c *= 7
+        elif ones > 4:
+            print('oops', ones)
+        ones = 0
+print(c)
