3cceed0491df0dcedfc35fcd2ba5445818cadeef
Alexey Shamrin <shamrin@gmail.com>
2020-12-17 11:23:56 +0200

    day 17 itertools.product

--- 17.py
+++ 17.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from itertools import product
 
 inp='''
 ...#..#.
@@ -15,7 +16,7 @@ g = {(x,y,0,0):'#' for y, row in enumerate(inp.split('\n')) for x, c in enumerat
 D = (-1,0,1)
 def add(v1, v2): return tuple(e1+e2 for e1, e2 in zip(v1,v2))
 def solve(g, Dw):
-    ds = [(dx,dy,dz,dw) for dx in D for dy in D for dz in D for dw in Dw if (dx,dy,dz,dw) != (0,0,0,0)]
+    ds = [d for d in product(D,D,D,Dw) if d != (0,0,0,0)]
     for _ in range(6):
         ns = Counter(add(p, d) for p in g for d in ds)
         g = {p:'#' for p, n in ns.items() if n == 3 or (n == 2 and p in g)}
