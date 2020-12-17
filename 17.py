a8dec7499a46f3bf73b80f2a28f642c548f1334c
Alexey Shamrin <shamrin@gmail.com>
2020-12-17 11:12:58 +0200

    day 17 tweak

--- 17.py
+++ 17.py
@@ -14,9 +14,7 @@ inp='''
 g = {(x,y,0,0):'#' for y, row in enumerate(inp.split('\n')) for x, c in enumerate(row) if c == '#'}
 D = (-1,0,1)
 
-def add(v1, v2):
-    return tuple(e1+e2 for e1, e2 in zip(v1,v2))
-
+def add(v1, v2): return tuple(e1+e2 for e1, e2 in zip(v1,v2))
 def solve(g, Dw):
     ds = [(dx,dy,dz,dw) for dx in D for dy in D for dz in D for dw in Dw if (dx,dy,dz,dw) != (0,0,0,0)]
     for _ in range(6):
