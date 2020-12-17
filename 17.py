3bb8d219ed4bf94725cd1dfb178c3cea8c222ac1
Alexey Shamrin <shamrin@gmail.com>
2020-12-17 11:28:14 +0200

    day 17 inline product

--- 17.py
+++ 17.py
@@ -16,9 +16,8 @@ g = {(x,y,0,0):'#' for y, row in enumerate(inp.split('\n')) for x, c in enumerat
 D = (-1,0,1)
 def add(v1, v2): return tuple(e1+e2 for e1, e2 in zip(v1,v2))
 def solve(g, Dw):
-    ds = [d for d in product(D,D,D,Dw) if d != (0,0,0,0)]
     for _ in range(6):
-        ns = Counter(add(p, d) for p in g for d in ds)
+        ns = Counter(add(p, d) for p in g for d in product(D,D,D,Dw) if d != (0,0,0,0))
         g = {p:'#' for p, n in ns.items() if n == 3 or (n == 2 and p in g)}
     return len(g)
 
