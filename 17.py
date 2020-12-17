69fdc5ef9cf23bf228f976c26531df302d7b9db3
Alexey Shamrin <shamrin@gmail.com>
2020-12-17 10:57:38 +0200

    day 17 tweak

--- 17.py
+++ 17.py
@@ -9,12 +9,9 @@ inp='''
 ........
 .#......
 ##...#..
-'''
-
-inp = inp.strip()
+'''.strip()
 
 D = (-1,0,1)
-
 def solve(Dw):
     ds = [(dx,dy,dz,dw) for dx in D for dy in D for dz in D for dw in Dw if (dx,dy,dz,dw) != (0,0,0,0)]
 
@@ -25,21 +22,13 @@ def solve(Dw):
                 g[x,y,0,0] = '#'
 
     for _ in range(6):
-        ng = {}
         ns = defaultdict(lambda: 0)
         for (x, y, z, w), c in g.items():
             for dx, dy, dz, dw in ds:
                 ns[x+dx, y+dy, z+dz, w+dw] += 1
-        for (x, y, z, w), n in ns.items():
-            if n == 3:
-                ng[x,y,z,w] = '#'
-            elif n == 2 and (x,y,z,w) in g:
-                ng[x,y,z,w] = '#'
-        g = ng
+        g = {(x,y,z,w):'#' for (x, y, z, w), n in ns.items() if n == 3 or (n == 2 and (x,y,z,w) in g)}
 
     return len(g)
 
-# part 1
 print(solve((0,)))
-# part 2
 print(solve(D))
