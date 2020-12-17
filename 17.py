447fb6056387302935aa247c25b3556ea684b9cc
Alexey Shamrin <shamrin@gmail.com>
2020-12-17 11:07:36 +0200

    day 17 Counter

--- 17.py
+++ 17.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter
 
 inp='''
 ...#..#.
@@ -12,22 +12,16 @@ inp='''
 '''.strip()
 
 D = (-1,0,1)
+
+def add(v1, v2):
+    return tuple(e1+e2 for e1, e2 in zip(v1,v2))
+
 def solve(Dw):
     ds = [(dx,dy,dz,dw) for dx in D for dy in D for dz in D for dw in Dw if (dx,dy,dz,dw) != (0,0,0,0)]
-
-    g = {}
-    for y, row in enumerate(inp.split('\n')):
-        for x, c in enumerate(row):
-            if c == '#':
-                g[x,y,0,0] = '#'
-
+    g = {(x,y,0,0):'#' for y, row in enumerate(inp.split('\n')) for x, c in enumerate(row) if c == '#'}
     for _ in range(6):
-        ns = defaultdict(lambda: 0)
-        for (x, y, z, w), c in g.items():
-            for dx, dy, dz, dw in ds:
-                ns[x+dx, y+dy, z+dz, w+dw] += 1
-        g = {(x,y,z,w):'#' for (x, y, z, w), n in ns.items() if n == 3 or (n == 2 and (x,y,z,w) in g)}
-
+        ns = Counter(add(p, d) for p in g for d in ds)
+        g = {p:'#' for p, n in ns.items() if n == 3 or (n == 2 and p in g)}
     return len(g)
 
 print(solve((0,)))
