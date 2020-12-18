6f0e106f138dfd4a99547e362f173a97a8bdd506
Alexey Shamrin <shamrin@gmail.com>
2020-12-18 18:14:12 +0200

    day 18 peekable

--- 18.py
+++ 18.py
@@ -2,68 +2,73 @@ import re
 
 inputs = [line.replace(' ', '').strip() for line in open('input18.txt') if line]
 
-class T:
-    def __init__(self, input, precedence):
-        self.input = input
-        self.i = 0
-        self.precedence = precedence
-        self.current = None
-        self.gen = self._gen()
-        self.next()
+class peekable:
+    def __init__(self, it):
+        self.it = iter(it)
+        self.cache = []
 
-    def next(self):
-        try:
-            self.current = next(self.gen)
-        except StopIteration:
-            self.current = None
-        return self.current
+    def peek(self):
+        if not self.cache:
+            try:
+                self.cache.append(next(self.it))
+            except StopIteration:
+                return None
+        return self.cache[-1]
 
-    def _gen(self):
-        while self.i < len(self.input):
-            c = self.input[self.i]
-            if c in '+*':
-                yield 'op', c, self.precedence[c]
-                self.i += 1
-            elif c in '()':
-                yield f'paren', c
-                self.i += 1
-            elif m := re.match(r'\d+', self.input[self.i:]):
-                yield 'num', int(m.group())
-                self.i += m.end()
-            else:
-                raise Exception(f'Unexpected token {self.input[self.i:]!r}')
+    def __next__(self):
+        if self.cache:
+            return self.cache.pop()
+        return next(self.it)
 
-    def __repr__(self):
-        return f'T(current={self.current}, I={self.input[self.i:]})'
+    def __iter__(self):
+        return self
+
+def tokens(input, precedence):
+    input = input.strip().replace(' ', '')
+    i = 0
+    while i < len(input):
+        c = input[i]
+        if c in '+*':
+            yield 'op', c, precedence[c]
+            i += 1
+        elif c in '(':
+            yield f'lparen', None
+            i += 1
+        elif c in ')':
+            yield f'rparen', None
+            i += 1
+        elif m := re.match(r'\d+', input[i:]):
+            yield 'num', int(m.group())
+            i += m.end()
+        else:
+            raise Exception(f'Unexpected token {input[i:]!r}')
 
 def atom(t):
-    tok, v = t.current
-    if (tok, v) == ('paren', '('):
-        t.next()
+    tok, v = t.peek()
+    if tok == 'lparen':
+        next(t)
         v = expr(t, 1)
-        assert t.current == ('paren', ')'), repr(t.current)
-        t.next()
+        assert t.peek()[0] == 'rparen', repr(t.peek())
+        next(t)
     elif tok == 'num':
-        t.next()
+        next(t)
     else:
-        raise Exception(f'Unexpected token {t.current!r} in atom: {t.I!r}')
+        raise Exception(f'Unexpected token {t.peek()!r}')
     return v
-
-def op(t):
-    return t.current, 1 if t.current[1] == '*' else 2
 
 def expr(t, min_prec):
     r = atom(t)
-    while t.current is not None and t.current[0] == 'op':
-        o, prec = t.current[1:]
+    while t.peek() is not None and t.peek()[0] == 'op':
+        o, prec = t.peek()[1:]
         if prec < min_prec:
             break
-        t.next()
+        next(t)
         rhs = expr(t, prec + 1) # +1 means left-assoc
         assert o in '+*', f'unexpected op {o!r}'
         r = (r * rhs) if o == '*' else (r + rhs)
     return r
 
-print(sum(expr(T(I, {'*': 1, '+': 1}), 1) for I in inputs))
-print(sum(expr(T(I, {'*': 1, '+': 2}), 1) for I in inputs))
-print(sum(expr(T(I, {'*': 2, '+': 1}), 1) for I in inputs))
+# print(expr(peekable(tokens('1 + 2 * 3 + 4 * 5 + 6', {'+': 1, '*': 1})), 1))
+
+for precedence in [{'*':1,'+':1}, {'*':1,'+':2}, {'*':2,'+':1}]:
+    print(sum(expr(peekable(tokens(I, precedence)), 1) for I in inputs))
